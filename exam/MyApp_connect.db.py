ac18c06270647dc85f62f753b60639317868aed8
Anna Kim <kimspb85@mail.ru>
2022-07-05 17:17:14 +0300

    update exam(emp)

--- exam/MyApp_connect.db.py
+++ exam/MyApp_connect.db.py
@@ -14,10 +14,15 @@ class Page1(QtWidgets.QMainWindow):
         self.ui.setupUi(self)
 
         self.ui.pushButtonCases.clicked.connect(self.onPushButtonCasesClicked)
+        self.ui.pushButtonEmp.clicked.connect(self.onPushButtonEmpClicked)
 
     def onPushButtonCasesClicked(self):
         self.cases = Cases()
         self.cases.show()
+
+    def onPushButtonEmpClicked(self):
+        self.emp = Employees()
+        self.emp.show()
 
 
 class Cases(QtWidgets.QMainWindow):
@@ -30,6 +35,8 @@ class Cases(QtWidgets.QMainWindow):
         self.ui.setupUi(self)
 
         self.initTableViewModel()
+
+        self.ui.pushButtonSave.clicked.connect(self.onPushButtonSaveClicked)
 
         # self.model = QtSql.QSqlRelationalTableModel()
         # self.model.setTable('litigation.cases')
@@ -51,8 +58,8 @@ class Cases(QtWidgets.QMainWindow):
         self.cursor = self.con.cursor()
 
     def initTableViewModel(self):
-        sim = QtGui.QStandardItemModel()
-        self.cursor.execute('SELECT LC.CaseNumber, LC.Court, PP.PersonName, LC.Claims, LT.TaskDateTime, SE.LastName '
+        self.sim = QtGui.QStandardItemModel()
+        self.cursor.execute('SELECT DISTINCT LC.CaseNumber, LC.Court, PP.PersonName, LC.Claims, LT.TaskDateTime, SE.LastName '
                             'FROM litigation.cases as LC '
                             'LEFT JOIN parties.case_participants as PCP '
                             'ON LC.id = PCP.CaseID '
@@ -72,10 +79,76 @@ class Cases(QtWidgets.QMainWindow):
             item4 = QtGui.QStandardItem(str(elem[3]))
             item5 = QtGui.QStandardItem(str(elem[4]))
             item6 = QtGui.QStandardItem(str(elem[5]))
-            sim.appendRow([item1, item2, item3, item4, item5, item6])
-        sim.setHorizontalHeaderLabels(['Номер дела', 'Суд', 'Истец', 'Требования', 'Дата заседания', 'Исполнитель'])
-        self.ui.tableViewClients.setModel(sim)
+            self.sim.appendRow([item1, item2, item3, item4, item5, item6])
+        self.sim.setHorizontalHeaderLabels(['Номер дела', 'Суд', 'Истец', 'Требования', 'Дата заседания', 'Исполнитель'])
+        self.ui.tableViewClients.setModel(self.sim)
+        self.ui.tableViewClients.setSelectionBehavior(QtWidgets.QAbstractItemView.SelectRows)
+        self.ui.tableViewClients.horizontalHeader().setSectionResizeMode(QtWidgets.QHeaderView.Stretch)
 
+        # self.ui.tableViewClients.selectionModel().currentChanged.connect(self.changecell)
+
+    # def changecell(self, cell):
+    #     self.ui.tableViewClients.commitData(self.ui.centralwidget)
+
+    def onPushButtonSaveClicked(self):
+        index = self.ui.tableViewClients.currentIndex()
+        value = self.sim.itemData(index)[0]
+        self.sim.setData(index, value)
+        self.sim.submit()
+        # self.ui.tableViewClients.update()
+        self.cursor.execute('UPDATE litigation.cases'
+                            f'SET Court = {str(value)}')
+        self.cursor.commit()
+        self.con.commit()
+
+
+class Employees(QtWidgets.QMainWindow):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self.initDB()
+
+        self.initUi()
+
+        self.initListViewModel()
+
+    def initUi(self):
+        self.centralWidget = QtWidgets.QWidget()
+        self.setCentralWidget(self.centralWidget)
+        self.window = QtWidgets.QTableView()
+        self.window.setWindowTitle('Сотрудники')
+
+        # self.setMinimumSize(500, 700)
+        # self.setMaximumSize(500, 700)
+
+
+        # self.model = QtSql.QSqlRelationalTableModel()
+        # self.model.setTable('litigation.cases')
+        # self.model.setRelation(3, QtSql.QSqlRelation('litigation.tasks', 'CaseID', 'TaskDateTime'))
+        # self.model.select()
+        # self.model.setEditStrategy(QSqlTableModel.OnManualSubmit)
+
+    def initDB(self):
+        server = 'vpngw.avalon.ru'
+        db = 'DevDB2022_KimAY'
+        user = 'tsqllogin'
+        pasw = 'Pa$$w0rd'
+        self.con = pyodbc.connect(
+            'DRIVER={ODBC Driver 17 for SQL Server}; '
+            'SERVER=' + server +
+            ';DATABASE=' + db +
+            ';UID=' + user +
+            ';PWD=' + pasw)
+        self.cursor = self.con.cursor()
+
+    def initListViewModel(self):
+        self.lm = QtSql.QSqlQueryModel(parent=self.window)
+        self.lm.setHeaderData(1, QtCore.Qt.Horizontal, 'Фамилия')
+        self.cursor.execute('SELECT LastName from [staff].[employees]')
+        data = self.cursor.fetchall()
+        print(data)
+
+        self.window.setModel(self.lm)
 
 
         # self.model = QtSql.QSqlTableModel()
