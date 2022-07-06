34e30bda9f54b36f253bd23232cb8e72b2b8f086
Anna Kim <kimspb85@mail.ru>
2022-07-07 02:16:20 +0300

    update exam(ui_cases)

--- exam/MyApp_connect.db.py
+++ exam/MyApp_connect.db.py
@@ -1,4 +1,6 @@
+import sys
 import pyodbc
+
 from PySide2 import QtWidgets, QtSql, QtCore, QtGui
 from PySide2.QtSql import QSqlQuery
 
@@ -6,7 +8,12 @@ from ui.pageCases1 import Ui_WindowCases
 from ui.page_1 import Ui_MainWindow
 from ui.emp import Ui_MainWindowEmp
 
+
+"""Начальная страница приложения. Позволяет выбрать представление информации в виде судебных дел либо сотрудников"""
+
+
 class Page1(QtWidgets.QMainWindow):
+
     def __init__(self, parent=None):
         super().__init__(parent)
 
@@ -16,6 +23,8 @@ class Page1(QtWidgets.QMainWindow):
         self.ui.pushButtonCases.clicked.connect(self.onPushButtonCasesClicked)
         self.ui.pushButtonEmp.clicked.connect(self.onPushButtonEmpClicked)
 
+    """Кнопки на начальной странице приложения "Дела", "Сотрудники" """
+
     def onPushButtonCasesClicked(self):
         self.cases = Cases()
         self.cases.show()
@@ -23,6 +32,21 @@ class Page1(QtWidgets.QMainWindow):
     def onPushButtonEmpClicked(self):
         self.emp = Employees()
         self.emp.show()
+
+    """Переопределение события закрытия окна"""
+
+    def closeEvent(self, event):
+        reply = QtWidgets.QMessageBox.question(self, "Закрыть окно?",
+                                               "Вы действительно хотите закрыть окно?",
+                                               QtWidgets.QMessageBox.Yes | QtWidgets.QMessageBox.No,
+                                               QtWidgets.QMessageBox.No)
+        if reply == QtWidgets.QMessageBox.Yes:
+            event.accept()
+        else:
+            event.ignore()
+
+
+"""Представление судебных дел"""
 
 
 class Cases(QtWidgets.QMainWindow):
@@ -37,12 +61,16 @@ class Cases(QtWidgets.QMainWindow):
         self.initTVModelCases()
 
         self.ui.pushButtonSave.clicked.connect(self.onPushButtonSaveClicked)
+        self.ui.addRow.triggered.connect(self.addRowTriggered)
+        self.ui.addColumn.triggered.connect(self.addColumnTriggered)
 
         # self.model = QtSql.QSqlRelationalTableModel()
         # self.model.setTable('litigation.cases')
         # self.model.setRelation(3, QtSql.QSqlRelation('litigation.tasks', 'CaseID', 'TaskDateTime'))
         # self.model.select()
         # self.model.setEditStrategy(QSqlTableModel.OnManualSubmit)
+
+    """Подключение к БД"""
 
     def initDB(self):
         server = 'vpngw.avalon.ru'
@@ -57,8 +85,13 @@ class Cases(QtWidgets.QMainWindow):
             ';PWD=' + pasw)
         self.cursor = self.con.cursor()
 
+    """Создание модели для отображения данных о судебных делах из БД в tableView"""
+
     def initTVModelCases(self):
+        # создание модели
         self.sim = QtGui.QStandardItemModel()
+
+        # запрос данных из БД
         self.cursor.execute('SELECT DISTINCT LC.CaseNumber, LC.Court, PP.PersonName, LC.Claims, LT.TaskDateTime, SE.LastName '
                             'FROM litigation.cases as LC '
                             'LEFT JOIN parties.case_participants as PCP '
@@ -72,6 +105,7 @@ class Cases(QtWidgets.QMainWindow):
         data = self.cursor.fetchall()
         # print(data)
 
+        # передача данных в модель
         for elem in data:
             item1 = QtGui.QStandardItem(str(elem[0]))
             item2 = QtGui.QStandardItem(str(elem[1]))
@@ -80,17 +114,27 @@ class Cases(QtWidgets.QMainWindow):
             item5 = QtGui.QStandardItem(str(elem[4]))
             item6 = QtGui.QStandardItem(str(elem[5]))
             self.sim.appendRow([item1, item2, item3, item4, item5, item6])
+
+        # установление заголовков столбцов модели
         self.sim.setHorizontalHeaderLabels(['Номер дела', 'Суд', 'Истец', 'Требования', 'Дата заседания', 'Исполнитель'])
+
+        # установка модели на tableView
         self.ui.tableViewClients.setModel(self.sim)
 
+        # выделение строки
         self.ui.tableViewClients.setSelectionBehavior(QtWidgets.QAbstractItemView.SelectRows)
 
+        # self.ui.tableViewClients.horizontalHeader().setSectionResizeMode(QtWidgets.QHeaderView.Stretch)
+
+        # установка размеров столбцов
         self.ui.tableViewClients.setColumnWidth(0, 80)
         self.ui.tableViewClients.setColumnWidth(1, 200)
         self.ui.tableViewClients.setColumnWidth(2, 150)
         self.ui.tableViewClients.setColumnWidth(3, 200)
         self.ui.tableViewClients.resizeRowsToContents()
+        # setAligment(QtCore.Qt.AlignHCenter)
 
+        # установка ширины основного окна
         self.centralWidget().setMinimumWidth(self.ui.tableViewClients.columnWidth(0) +
                                              self.ui.tableViewClients.columnWidth(1) +
                                              self.ui.tableViewClients.columnWidth(2) +
@@ -98,26 +142,51 @@ class Cases(QtWidgets.QMainWindow):
                                              self.ui.tableViewClients.columnWidth(4) +
                                              self.ui.tableViewClients.columnWidth(5) + 50)
 
-        # self.ui.tableViewClients.horizontalHeader().setStretchLastSection(True)
-        # self.ui.tableViewClients.horizontalHeader().setSectionResizeMode(QtWidgets.QHeaderView.Stretch)
 
-        # self.ui.tableViewClients.selectionModel().currentChanged.connect(self.changecell)
-
-    # def changecell(self, cell):
-    #     self.ui.tableViewClients.commitData(self.ui.centralwidget)
+    # Кнопка сохранения данных в БД - ОНА НЕ СРАБАТЫВАЕТ, НИЧЕГО НЕ СОХРАНЯЕТСЯ.
+    # Ниже в классе Employees попыталась сделать через другую модель (не QItemModel, а SQLTableModel).
+    # Были такие примеры на лекциях. Но я не понимаю, почему можно использовать совершенно разные модели из разных
+    # классов (первый из QtGui - расширяет возможности графического интерфейса, а второй - QtSQL).
+    # Второй вариант через QtSQL у меня не то, что не работает, а еще даже и не выводит данные в приложение,
+    # только печатает в консоли
 
     def onPushButtonSaveClicked(self):
         index = self.ui.tableViewClients.currentIndex()
         value = self.sim.itemData(index)[0]
         self.sim.setData(index, value)
         self.sim.submit()
+        print(value)
         # self.ui.tableViewClients.update()
         self.cursor.execute('UPDATE litigation.cases'
                             f'SET Court = {str(value)}')
         self.cursor.commit()
         self.con.commit()
 
-    # def changeEvent(self, event: QtCore.QEvent) -> None:
+    # слоты для меню
+    @QtCore.Slot()
+    def addRowTriggered(self):
+        self.sim.insertRow(self.sim.rowCount(), [])
+
+    @QtCore.Slot()
+    def addColumnTriggered(self):
+        self.sim.insertColumn(self.sim.rowCount(), [])
+
+    """Переопределение события закрытия окна"""
+    def closeEvent(self, event):
+        reply = QtWidgets.QMessageBox.question(self, "Закрыть окно?",
+                                               "Вы действительно хотите закрыть окно?",
+                                               QtWidgets.QMessageBox.Yes | QtWidgets.QMessageBox.No,
+                                               QtWidgets.QMessageBox.No)
+        if reply == QtWidgets.QMessageBox.Yes:
+            event.accept()
+        else:
+            event.ignore()
+
+
+""""Представление сотрудников"""
+
+
+"""Переопределение параметров модели"""
 
 
 class EditableSQLModel(QtSql.QSqlTableModel):
@@ -128,6 +197,9 @@ class EditableSQLModel(QtSql.QSqlTableModel):
         if role == QtCore.Qt.BackgroundRole:
             if item.row() % 2:
                 return QtGui.QColor(QtCore.Qt.blue)
+
+
+"""Представление сотрудников"""
 
 
 class Employees(QtWidgets.QMainWindow):
@@ -141,25 +213,7 @@ class Employees(QtWidgets.QMainWindow):
 
         self.initSQLModel()
 
-        # self.initUi()
-
-
-    # def initUi(self):
-    #     self.window = QtWidgets.QWidget()
-    #     self.window.setWindowTitle('Сотрудники')
-    #
-    #     self.vbox = QtWidgets.QVBoxLayout()
-    #     self.tv = QtWidgets.QTableView()
-    #     self.vbox.addWidget(self.tv)
-    #
-    #     self.window.setLayout(self.vbox)
-
-
-        # self.model = QtSql.QSqlRelationalTableModel()
-        # self.model.setTable('litigation.cases')
-        # self.model.setRelation(3, QtSql.QSqlRelation('litigation.tasks', 'CaseID', 'TaskDateTime'))
-        # self.model.select()
-        # self.model.setEditStrategy(QSqlTableModel.OnManualSubmit)
+    """Подключение к БД"""
 
     def initDB(self):
         server = 'vpngw.avalon.ru'
@@ -174,10 +228,16 @@ class Employees(QtWidgets.QMainWindow):
             ';PWD=' + pasw)
         self.cursor = self.con.cursor()
 
+    """Создание модели для отображения данных о сотрудниках из БД в tableView"""
+
     def initSQLModel(self):
+
+        """создание модели"""
         self.empModel = EditableSQLModel()
-        # self.empModel = QtSql.QSqlTableModel()
+        # self.empModel = QtSql.QSqlTableModel() - ВАРИАНТ БЕЗ ПЕРЕОПРЕДЕЛЕНИЯ МОДЕЛИ
         self.empModel.setEditStrategy(QtSql.QSqlTableModel.OnManualSubmit)
+
+        """Подключение к таблице с данными для отображения в модели"""
         self.empModel.setTable('[staff].[employees]')
         print(self.cursor.execute('SELECT * from [staff].[employees]').fetchall())
         self.empModel.select()
@@ -188,55 +248,6 @@ class Employees(QtWidgets.QMainWindow):
         self.empModel.setHeaderData(3, QtCore.Qt.Horizontal, 'Должность')
 
         self.ui.tableViewEmp.setModel(self.empModel)
-
-        # self.cursor.execute('SELECT LastName from [staff].[employees]')
-        # data = self.cursor.fetchall()
-        # print(data)
-        # self.tv.setModel(self.empModel)
-
-
-        # index = self.empModel.rowCount()
-        # self.empModel.setData(self.empModel.index(index, 0), data[0][0])
-        # self.empModel.setData(self.empModel.index(index, 0), data[1][0])
-        # for elem in data:
-        #     print(elem)
-            # item1, item2 = elem[0], elem[1]
-        # print(item1, item2)
-
-
-        # index = self.model.rowCount()
-        # query = QSqlQuery()
-        # query.exec_('select * from litigation.cases')
-        # query.exec_('SELECT LC.CaseNumber, LC.Court, PP.PersonName, LC.Claims, LT.TaskDateTime, SE.LastName '
-        #            'FROM litigation.cases as LC '
-        #            'LEFT JOIN parties.case_participants as PCP '
-        #            'ON LC.id = PCP.CaseID '
-        #            'LEFT JOIN parties.persons as PP '
-        #            'ON PCP.PersonID = PP.id '
-        #            'LEFT JOIN staff.employees as SE '
-        #            'ON LC.DoerID = SE.id '
-        #            'JOIN litigation.tasks as LT '
-        #            'on LC.id = LT.CaseID')
-
-        # for elem in query:
-        #     item1 = QtSql.QSqlQuery.value(self, 1)
-        #     item2 = QtSql.QSqlQuery.value(self, 2)
-        #     item3 = QtSql.QSqlQuery.value(self, 3)
-        #     item4 = QtSql.QSqlQuery.value(self, 4)
-        #     item5 = QtSql.QSqlQuery.value(self, 5)
-        #     item6 = QtSql.QSqlQuery.value(self, 6)
-        #     print(item1, item2, item3, item4, item5, item6)item6
-
-        # print(query.value('CaseNumber'))
-
-        # self.model.setData(self.model.index(index, 1), item1)
-        # self.model.setData(self.model.index(index, 2), item2)
-        # self.model.setData(self.model.index(index, 3), item3)
-        # self.model.setData(self.model.index(index, 4), item4)
-        # self.model.setData(self.model.index(index, 5), item5)
-        # self.model.setData(self.model.index(index, 6), item6)
-
-        # self.ui.tableViewClients.setModel(self.model)
 
 
 if __name__ == "__main__":
